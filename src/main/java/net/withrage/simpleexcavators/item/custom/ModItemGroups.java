31c876974defdad72591ad58d44cd90fa8430b2b
justerik <sebo.erik@gmail.com>
2025-11-07 16:10:02 +0100

    ported everything to match the 1.20.6 version

---
+++ src/main/java/net/withrage/simpleexcavators/item/custom/ModItemGroups.java
@@ -0,0 +1,30 @@
+package net.withrage.simpleexcavators.item.custom;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.withrage.simpleexcavators.SimpleExcavators;
+
+public class ModItemGroups {
+    public static final ItemGroup SIMPLE_EXCAVATORS = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(SimpleExcavators.MOD_ID, "iron_excavator"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.simpleexcavators"))
+                    .icon(() -> new ItemStack(ModItems.IRON_EXCAVATOR)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.WOODEN_EXCAVATOR);
+                        entries.add(ModItems.STONE_EXCAVATOR);
+                        entries.add(ModItems.COPPER_EXCAVATOR);
+                        entries.add(ModItems.GOLDEN_EXCAVATOR);
+                        entries.add(ModItems.IRON_EXCAVATOR);
+                        entries.add(ModItems.EMERALD_EXCAVATOR);
+                        entries.add(ModItems.DIAMOND_EXCAVATOR);
+                        entries.add(ModItems.NETHERITE_EXCAVATOR);
+                    }).build());
+
+    public static void registerItemGroups() {
+        SimpleExcavators.LOGGER.info("Registering Item Groups for " + SimpleExcavators.MOD_ID);
+    }
+}
---
+++ src/main/java/net/withrage/simpleexcavators/item/custom/ModItems.java
@@ -0,0 +1,41 @@
+package net.withrage.simpleexcavators.item.custom;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.withrage.simpleexcavators.SimpleExcavators;
+
+public class ModItems {
+    public static final Item WOODEN_EXCAVATOR = registerExcavator("wooden_excavator", ModToolMaterial.WOOD, 2, -2.8F, 108, false);
+    public static final Item STONE_EXCAVATOR = registerExcavator("stone_excavator", ModToolMaterial.STONE, 3, -2.8F, 262, false);
+    public static final Item COPPER_EXCAVATOR = registerExcavator("copper_excavator", ModToolMaterial.COPPER, 3, -2.8F, 380, false);
+    public static final Item GOLDEN_EXCAVATOR = registerExcavator("golden_excavator", ModToolMaterial.GOLD, 2, -2.8F, 64, false);
+    public static final Item IRON_EXCAVATOR = registerExcavator("iron_excavator", ModToolMaterial.IRON, 4, -2.8F, 506, false);
+    public static final Item EMERALD_EXCAVATOR = registerExcavator("emerald_excavator", ModToolMaterial.EMERALD, 5, -2.8F, 2084, false);
+    public static final Item DIAMOND_EXCAVATOR = registerExcavator("diamond_excavator", ModToolMaterial.DIAMOND, 5, -2.8F, 3122, false);
+    public static final Item NETHERITE_EXCAVATOR = registerExcavator("netherite_excavator", ModToolMaterial.NETHERITE, 6, -2.8F, 4062, true);
+
+    private static Item registerExcavator(String name,
+                                       ToolMaterial material,
+                                       int attackDamage,
+                                       float attackSpeed,
+                                       int durability,
+                                       boolean fireproof) {
+
+        Item.Settings settings = new Item.Settings()
+                .maxCount(1)
+                .maxDamage(durability);
+
+        if (fireproof) settings.fireproof();
+
+        return Registry.register(Registries.ITEM,
+                new Identifier("simpleexcavators", name),
+                new ExcavatorItem(material, attackDamage, attackSpeed, settings));
+    }
+
+    public static void registerModItems() {
+        SimpleExcavators.LOGGER.info("Registering Mod Items for " + SimpleExcavators.MOD_ID);
+    }
+}
