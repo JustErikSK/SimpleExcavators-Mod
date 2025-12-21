cbc60e17178d7c83d0bb0b5529de37db41777e8e
justerik <sebo.erik@gmail.com>
2025-12-21 11:13:33 +0100

    ported v1.0.1 to 1.20.4

--- src/main/java/net/withrage/simpleexcavators/item/custom/ExcavatorItem.java
+++ src/main/java/net/withrage/simpleexcavators/item/custom/ExcavatorItem.java
@@ -2,6 +2,7 @@ package net.withrage.simpleexcavators.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -13,6 +14,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.withrage.simpleexcavators.config.SimpleExcavatorsConfig;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -46,7 +48,7 @@ public class ExcavatorItem extends MiningToolItem {
             if (!state.isIn(BlockTags.SHOVEL_MINEABLE)) {
                 return result;
             }
-            if (player.isSneaking()) {
+            if (SimpleExcavatorsConfig.sneakMines1x1 && player.isSneaking()) {
                 return result;
             }
             Direction hitFace = ExcavatorMiningContext.consumeLastHitFace(player);
@@ -137,6 +139,28 @@ public class ExcavatorItem extends MiningToolItem {
                 net.minecraft.block.Blocks.AIR.getDefaultState(),
                 Block.NOTIFY_ALL
         );
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(stack, world, entity, slot, selected);
+
+        if (world.isClient()) return;
+        if (!(entity instanceof PlayerEntity)) return;
+
+        int max = stack.getMaxDamage();
+        if (max <= 0) return;
+
+        int dmg = stack.getDamage();
+
+        if (dmg < 0) {
+            stack.setDamage(0);
+            return;
+        }
+
+        if (dmg >= max) {
+            stack.setDamage(max - 1);
+        }
     }
 
     private Plane getPlaneFromHitFace(Direction face) {
--- src/main/java/net/withrage/simpleexcavators/item/custom/ModItems.java
+++ src/main/java/net/withrage/simpleexcavators/item/custom/ModItems.java
@@ -1,20 +1,31 @@
 package net.withrage.simpleexcavators.item.custom;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.withrage.simpleexcavators.SimpleExcavators;
+import net.withrage.simpleexcavators.config.SimpleExcavatorsConfig;
 
 public class ModItems {
-    public static final Item WOODEN_EXCAVATOR = registerItem("wooden_excavator", new ExcavatorItem(ModToolMaterial.WOOD, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(108)));
-    public static final Item STONE_EXCAVATOR = registerItem("stone_excavator", new ExcavatorItem(ModToolMaterial.STONE, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(262)));
-    public static final Item COPPER_EXCAVATOR = registerItem("copper_excavator", new ExcavatorItem(ModToolMaterial.COPPER, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(380)));
-    public static final Item GOLDEN_EXCAVATOR = registerItem("golden_excavator", new ExcavatorItem(ModToolMaterial.GOLD, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(64)));
-    public static final Item IRON_EXCAVATOR = registerItem("iron_excavator", new ExcavatorItem(ModToolMaterial.IRON, 4, -2.8F, new Item.Settings().maxCount(1).maxDamage(506)));
-    public static final Item EMERALD_EXCAVATOR = registerItem("emerald_excavator", new ExcavatorItem(ModToolMaterial.EMERALD, 5, -2.8F, new Item.Settings().maxCount(1).maxDamage(2084)));
-    public static final Item DIAMOND_EXCAVATOR = registerItem("diamond_excavator", new ExcavatorItem(ModToolMaterial.DIAMOND, 5, -2.8F, new Item.Settings().maxCount(1).maxDamage(3122)));
-    public static final Item NETHERITE_EXCAVATOR = registerItem("netherite_excavator", new ExcavatorItem(ModToolMaterial.NETHERITE, 6, -2.8F, new Item.Settings().maxCount(1).maxDamage(4062)));
+    public static final Item WOODEN_EXCAVATOR = registerItem("wooden_excavator", new ExcavatorItem(ModToolMaterial.WOOD, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.woodenDurability)));
+    public static final Item STONE_EXCAVATOR = registerItem("stone_excavator", new ExcavatorItem(ModToolMaterial.STONE, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.stoneDurability)));
+    public static final Item COPPER_EXCAVATOR = registerItem("copper_excavator", new ExcavatorItem(ModToolMaterial.COPPER, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.copperDurability)));
+    public static final Item GOLDEN_EXCAVATOR = registerItem("golden_excavator", new ExcavatorItem(ModToolMaterial.GOLD, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.goldenDurability)));
+    public static final Item IRON_EXCAVATOR = registerItem("iron_excavator", new ExcavatorItem(ModToolMaterial.IRON, 4, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.ironDurability)));
+    public static final Item EMERALD_EXCAVATOR = registerItem("emerald_excavator", new ExcavatorItem(ModToolMaterial.EMERALD, 5, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.emeraldDurability)));
+    public static final Item DIAMOND_EXCAVATOR = registerItem("diamond_excavator", new ExcavatorItem(ModToolMaterial.DIAMOND, 5, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.diamondDurability)));
+    public static final Item NETHERITE_EXCAVATOR = registerItem("netherite_excavator", new ExcavatorItem(ModToolMaterial.NETHERITE, 6, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.netheriteDurability)));
+
+    public static final Item EXCAVATOR_HANDLE = registerItem("excavator_handle", new Item(new FabricItemSettings()));
+    public static final Item WOODEN_EXCAVATOR_HEAD = registerItem("wooden_excavator_head", new Item(new FabricItemSettings()));
+    public static final Item STONE_EXCAVATOR_HEAD = registerItem("stone_excavator_head", new Item(new FabricItemSettings()));
+    public static final Item COPPER_EXCAVATOR_HEAD = registerItem("copper_excavator_head", new Item(new FabricItemSettings()));
+    public static final Item GOLDEN_EXCAVATOR_HEAD = registerItem("golden_excavator_head", new Item(new FabricItemSettings()));
+    public static final Item IRON_EXCAVATOR_HEAD = registerItem("iron_excavator_head", new Item(new FabricItemSettings()));
+    public static final Item EMERALD_EXCAVATOR_HEAD = registerItem("emerald_excavator_head", new Item(new FabricItemSettings()));
+    public static final Item DIAMOND_EXCAVATOR_HEAD = registerItem("diamond_excavator_head", new Item(new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(SimpleExcavators.MOD_ID, name), item);
