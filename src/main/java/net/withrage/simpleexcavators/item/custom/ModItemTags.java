112261eb218f82099a92b38f2581296767123828
justerik <sebo.erik@gmail.com>
2025-12-23 18:16:11 +0100

    ported v1.0.1 to 1.21.10

--- src/main/java/net/withrage/simpleexcavators/item/custom/ModItemTags.java
+++ src/main/java/net/withrage/simpleexcavators/item/custom/ModItemTags.java
@@ -6,7 +6,21 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public final class ModItemTags {
+    public static final TagKey<Item> WOODEN_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simpleexcavators", "wooden_tool_materials"));
+    public static final TagKey<Item> STONE_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simpleexcavators", "stone_tool_materials"));
+    public static final TagKey<Item> COPPER_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simpleexcavators", "copper_tool_materials"));
+    public static final TagKey<Item> GOLDEN_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simpleexcavators", "golden_tool_materials"));
+    public static final TagKey<Item> IRON_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simpleexcavators", "iron_tool_materials"));
     public static final TagKey<Item> EMERALD_TOOL_MATERIALS =
             TagKey.of(RegistryKeys.ITEM, Identifier.of("simpleexcavators", "emerald_tool_materials"));
+    public static final TagKey<Item> DIAMOND_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simpleexcavators", "diamond_tool_materials"));
+    public static final TagKey<Item> NETHERITE_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simpleexcavators", "netherite_tool_materials"));
     private ModItemTags() {}
 }
--- src/main/java/net/withrage/simpleexcavators/item/custom/ModToolMaterial.java
+++ src/main/java/net/withrage/simpleexcavators/item/custom/ModToolMaterial.java
@@ -2,23 +2,88 @@ package net.withrage.simpleexcavators.item.custom;
 
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.tag.BlockTags;
+import net.withrage.simpleexcavators.config.SimpleExcavatorsConfig;
+import net.withrage.simpleexcavators.item.custom.ModItemTags;
 
 public class ModToolMaterial {
-    public static final ToolMaterial WOOD = ToolMaterial.WOOD;
-    public static final ToolMaterial STONE = ToolMaterial.STONE;
-    public static final ToolMaterial COPPER = ToolMaterial.COPPER;
-    public static final ToolMaterial GOLD = ToolMaterial.GOLD;
-    public static final ToolMaterial IRON = ToolMaterial.IRON;
-    public static final ToolMaterial DIAMOND = ToolMaterial.DIAMOND;
-    public static final ToolMaterial NETHERITE = ToolMaterial.NETHERITE;
+
+    public static final ToolMaterial WOOD =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+                    SimpleExcavatorsConfig.woodenDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.WOODEN_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial STONE =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_STONE_TOOL,
+                    SimpleExcavatorsConfig.stoneDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.STONE_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial COPPER =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_IRON_TOOL,
+                    SimpleExcavatorsConfig.copperDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.COPPER_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial GOLD =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_GOLD_TOOL,
+                    SimpleExcavatorsConfig.goldenDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.GOLDEN_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial IRON =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_IRON_TOOL,
+                    SimpleExcavatorsConfig.ironDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.IRON_TOOL_MATERIALS
+            );
 
     public static final ToolMaterial EMERALD =
             new ToolMaterial(
                     BlockTags.INCORRECT_FOR_DIAMOND_TOOL,
-                    2031,
+                    SimpleExcavatorsConfig.emeraldDurability,
                     8.0f,
                     3.0f,
                     15,
                     ModItemTags.EMERALD_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial DIAMOND =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_DIAMOND_TOOL,
+                    SimpleExcavatorsConfig.diamondDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.DIAMOND_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial NETHERITE =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_NETHERITE_TOOL,
+                    SimpleExcavatorsConfig.netheriteDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.NETHERITE_TOOL_MATERIALS
             );
 }
