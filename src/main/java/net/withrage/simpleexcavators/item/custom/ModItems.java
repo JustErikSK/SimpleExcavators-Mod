3f78d79e5a94676bf6e22122d00886f90ba0ab99
justerik <sebo.erik@gmail.com>
2026-04-08 14:21:01 +0200

    added advancement root file, added config option for 3x3 path making, changed the attack speed of all excavators

--- src/main/java/net/withrage/simpleexcavators/item/custom/ModItems.java
+++ src/main/java/net/withrage/simpleexcavators/item/custom/ModItems.java
@@ -9,14 +9,14 @@ import net.withrage.simpleexcavators.SimpleExcavators;
 import net.withrage.simpleexcavators.config.SimpleExcavatorsConfig;
 
 public class ModItems {
-    public static final Item WOODEN_EXCAVATOR = registerItem("wooden_excavator", new ExcavatorItem(ModToolMaterial.WOOD, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.woodenDurability)));
-    public static final Item STONE_EXCAVATOR = registerItem("stone_excavator", new ExcavatorItem(ModToolMaterial.STONE, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.stoneDurability)));
-    public static final Item COPPER_EXCAVATOR = registerItem("copper_excavator", new ExcavatorItem(ModToolMaterial.COPPER, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.copperDurability)));
-    public static final Item GOLDEN_EXCAVATOR = registerItem("golden_excavator", new ExcavatorItem(ModToolMaterial.GOLD, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.goldenDurability)));
-    public static final Item IRON_EXCAVATOR = registerItem("iron_excavator", new ExcavatorItem(ModToolMaterial.IRON, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.ironDurability)));
-    public static final Item EMERALD_EXCAVATOR = registerItem("emerald_excavator", new ExcavatorItem(ModToolMaterial.EMERALD, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.emeraldDurability)));
-    public static final Item DIAMOND_EXCAVATOR = registerItem("diamond_excavator", new ExcavatorItem(ModToolMaterial.DIAMOND, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.diamondDurability)));
-    public static final Item NETHERITE_EXCAVATOR = registerItem("netherite_excavator", new ExcavatorItem(ModToolMaterial.NETHERITE, 4, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.netheriteDurability).fireproof()));
+    public static final Item WOODEN_EXCAVATOR = registerItem("wooden_excavator", new ExcavatorItem(ModToolMaterial.WOOD, 1, -3.2F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.woodenDurability)));
+    public static final Item STONE_EXCAVATOR = registerItem("stone_excavator", new ExcavatorItem(ModToolMaterial.STONE, 1, -3.1F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.stoneDurability)));
+    public static final Item COPPER_EXCAVATOR = registerItem("copper_excavator", new ExcavatorItem(ModToolMaterial.COPPER, 1, -2.9F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.copperDurability)));
+    public static final Item GOLDEN_EXCAVATOR = registerItem("golden_excavator", new ExcavatorItem(ModToolMaterial.GOLD, 1, -2.7F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.goldenDurability)));
+    public static final Item IRON_EXCAVATOR = registerItem("iron_excavator", new ExcavatorItem(ModToolMaterial.IRON, 2, -3.0F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.ironDurability)));
+    public static final Item EMERALD_EXCAVATOR = registerItem("emerald_excavator", new ExcavatorItem(ModToolMaterial.EMERALD, 3, -2.5F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.emeraldDurability)));
+    public static final Item DIAMOND_EXCAVATOR = registerItem("diamond_excavator", new ExcavatorItem(ModToolMaterial.DIAMOND, 3, -2.6F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.diamondDurability)));
+    public static final Item NETHERITE_EXCAVATOR = registerItem("netherite_excavator", new ExcavatorItem(ModToolMaterial.NETHERITE, 4, -2.4F, new Item.Settings().maxCount(1).maxDamage(SimpleExcavatorsConfig.netheriteDurability).fireproof()));
 
     public static final Item WOODEN_HANDLE = registerItem("wooden_handle", new Item(new FabricItemSettings()));
     public static final Item WOODEN_EXCAVATOR_HEAD = registerItem("wooden_excavator_head", new Item(new FabricItemSettings()));
